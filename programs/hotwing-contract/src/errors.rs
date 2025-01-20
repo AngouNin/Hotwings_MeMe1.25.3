fb974321676be224082e411727e67db30d76086c
AngouNin <bmars8218@gmail.com>
2025-01-21 05:07:00 +0900

    File distribution

---
+++ programs/hotwing-contract/src/errors.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+
+// Error codes
+#[error_code]
+pub enum ErrorCode {
+    #[msg("User already registered")]
+    UserAlreadyRegistered,
+    #[msg("Max users reached")]
+    MaxUsersReached,
+    #[msg("Token account creation failed")]
+    TokenAccountCreationFailed,
+    #[msg("Arithmetic overflow occurred")]
+    ArithmeticOverflow,
+    #[msg("User not found")]
+    UserNotFound, 
+    #[msg("User's associated token account (ATA) not found in remaining accounts.")]
+    UserWalletNotFound, 
+    #[msg("User's accounts not enough in remaining accounts.")]
+    AccountNotEnough,
+    #[msg("Token transfer failed")]
+    TokenTransferFailed,
+    #[msg("Next milestone not reached yet")]
+    MiletoneNotReached,
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+    #[msg("Invalid burn wallet account")]
+    InvalidBurnWallet,
+    #[msg("Invalid marketing wallet account")]
+    InvalidMarketingWallet,
+    #[msg("Invalid project wallet account")]
+    InvalidProjectWallet,
+    #[msg("Deserialization failed")]
+    DeserializationFailed,
+    #[msg("Serialization failed")]
+    SerializationFailed,
+}
